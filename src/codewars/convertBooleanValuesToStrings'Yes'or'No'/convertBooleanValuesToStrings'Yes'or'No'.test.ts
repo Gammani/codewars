d88daa82adfc3948175135dbd8ae2cf1b065cd0c
Oreda-Z <shtucer31@gmail.com>
2020-12-21 21:36:06 +0300

    Convert boolean values to strings 'Yes' or 'No'

---
+++ src/codewars/convertBooleanValuesToStrings'Yes'or'No'/convertBooleanValuesToStrings'Yes'or'No'.test.ts
@@ -0,0 +1,17 @@
+import {boolToWord} from "./convertBooleanValuesToStrings'Yes'or'No'";
+
+let booleanValue1: boolean;
+let booleanValue2: boolean;
+
+beforeEach(() => {
+    booleanValue1 = true;
+    booleanValue2 = false;
+})
+
+test('should Convert boolean values to strings Yes or No', () => {
+    let result1 = boolToWord(booleanValue1);
+    let result2 = boolToWord(booleanValue2);
+
+    expect(result1).toBe("Yes");
+    expect(result2).toBe("No");
+})
--- src/codewars/grasshopperSummation/grasshopperSummation.test.ts
+++ src/codewars/grasshopperSummation/grasshopperSummation.test.ts
@@ -2,16 +2,20 @@ import {summation} from "./grasshopperSummation";
 
 let someNumber1: number;
 let someNumber2: number;
+let someNumber3: number;
 
 beforeEach(() => {
     someNumber1 = 7;
     someNumber2 = 1;
+    someNumber3 = 0;
 })
 
 test('should be Grasshopper - Summation', () => {
     let result1 = summation(someNumber1);
     let result2 = summation(someNumber2);
+    let result3 = summation(someNumber3);
 
     expect(result1).toBe(28);
     expect(result2).toBe(1);
+    expect(result3).toBe(0);
 })
